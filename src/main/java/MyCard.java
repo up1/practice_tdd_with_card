2e40bc4a86e760bf74794c914b9b2c38746ed827
somkiat <somkiat.p@gmail.com>
2017-09-11 19:24:37 +0700

    Improve readability of test case

--- src/main/java/MyCard.java
+++ src/main/java/MyCard.java
@@ -3,6 +3,6 @@ public class MyCard {
     public String cartAt(int position) {
         String ดอกไพ่ = "C";
         String[] เลขไพ่ = {"2", "3", "4", "5", "6", "7", "8", "9", "0", "J", "Q", "K", "A"};
-        return String.format("%s%s", เลขไพ่[position], ดอกไพ่);
+        return String.format("%s%s", เลขไพ่[position-1], ดอกไพ่);
     }
 }
--- src/test/java/MyCardTest.java
+++ src/test/java/MyCardTest.java
@@ -8,27 +8,32 @@ public class MyCardTest {
 
     @Test public void
     ไพ่ใบแรกในสำรับต้องเป็น_2C() {
-        assertEquals("2C", myCard.cartAt(0));
+        assertEquals("2C", myCard.cartAt(1));
     }
 
     @Test public void
     ไพ่ใบที่สองในสำรับต้องเป็น_3C() {
-        assertEquals("3C", myCard.cartAt(1));
+        assertEquals("3C", myCard.cartAt(2));
     }
 
     @Test public void
     ไพ่ใบที่สามในสำรับต้องเป็น_4C() {
-        assertEquals("4C", myCard.cartAt(2));
+        assertEquals("4C", myCard.cartAt(3));
     }
 
     @Test public void
     ไพ่ใบที่สี่ในสำรับต้องเป็น_4C() {
-        assertEquals("5C", myCard.cartAt(3));
+        assertEquals("5C", myCard.cartAt(4));
     }
 
     @Test public void
     ไพ่ใบที่เก้าในสำรับต้องเป็น_0C() {
-        assertEquals("0C", myCard.cartAt(8));
+        assertEquals("0C", myCard.cartAt(9));
+    }
+
+    @Test public void
+    ไพ่ใบที่สิบสามในสำรับต้องเป็น_AC() {
+        assertEquals("AC", myCard.cartAt(13));
     }
 
 }
